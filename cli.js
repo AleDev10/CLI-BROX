2388532fe5072e32c84addabf9f1c771cba6e04a
Alexandre Junqueiro <tchunaartur@gmail.com>
2025-07-25 00:24:53 +0100

    cls1

--- cli.js
+++ cli.js
@@ -1,19 +1,68 @@
 #!/usr/bin/env node
 
 //importações
-const readline = require('readline');
-const inquirer = require('inquirer');
+const readline = require('readline')
+const inquirer = require("@inquirer/prompts");
+const yp = require("yargs-parser");
+const giz = require("chalk");
+const figlet = require("figlet");
+const { stdin, stdout } = require('process');
 
 const cli = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,
-    prompt:"BROX>",
+    input:stdin,
+    output:stdout,
+    prompt:giz.white.bgRed('BROX@§:')
 });
 
-cli.prompt();
+async function pergunta() {
+    await inquirer.input({ message: "Enter your name" })
+    try {
+        cli.prompt();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-cli.on(()=>{
-    process.exit(0);
-})
+async function iniciarCLI() {
+  console.clear();
 
-console.log("Inicio do projeto.");
+  console.log(
+    await figlet.text("BROX", {
+      font: "Standard",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    })
+  );
+  try {
+    console.log(giz.italic("O centro de todas as operações"))
+    cli.prompt();
+  } catch (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+  }
+}
+
+cli.on("line",(data)=>{
+    if (data==='') {
+        cli.prompt();
+    } else {
+        let parse = yp(data);
+        let [comando,...args]= parse._;
+        console.log('comando: '+comando);
+        console.log('args: '+args);
+        console.log('flags: '+parse);
+        cli.prompt();
+        if (comando=='pf') {
+            pergunta();
+        }
+    }
+});
+
+cli.on("close",()=>{
+    process.exit(0)
+});
+
+
+iniciarCLI();
