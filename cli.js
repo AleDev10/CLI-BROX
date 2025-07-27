30848e748212bc9f9201021d7068df862c927328
Alexandre Junqueiro <tchunaartur@gmail.com>
2025-07-28 00:38:00 +0100

    titulo

--- cli.js
+++ cli.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
 const blessed = require("blessed");
+const contrib = require('blessed-contrib');
 const giz = require("chalk");
+const { fonts } = require("figlet");
+const figlet = require('figlet');
 
 /*configurações gerais*/
 const screen = blessed.screen({
   smartCSR: true,
+  title: 'CLI BROX'
 });
+
+let texto;
+
+figlet.text('HELLO', { font: 'Ghost' }, function (err, data) {
+  if (err) throw err;
+  texto = data;
+});
+
 
 /*elementos*/
 const box = blessed.box({
@@ -17,61 +29,130 @@ const box = blessed.box({
   border: { type: "line" },
   style: {
     fg: "white",
-    bg: "blue",
+    bg: "#262626",
   },
 });
 
-const input = blessed.textbox({
-  top: '100%-5',
+const boxPrompt = blessed.box({
+  parent: box,
+  bottom: 1,
   left: "2%",
   width: "50%",
   height: 3,
+  label:'Prompt',
   border: { type: "line" },
+  style:{
+    border:{
+      fg:'#F21B2D'
+    }
+  }
+});
+
+const input = blessed.textbox({
+  parent: boxPrompt,
+  top: 0,
+  left: "15%",
+  width: '70%',
+  height: 1,
+  border: false,
   inputOnFocus: true, // Permite digitar ao focar
-  value: giz.white.bgGreen.bold("BROX@>"),
+  style:{
+    fg: '#D9D9D9',
+  }
+});
+
+const textPrompt = blessed.text({
+  parent: boxPrompt,
+  top: 0,
+  left: "1%",
+  width: 'shrink',
+  height: 1,
+  border: false,
+  content: giz.hex('#D9D9D9').bold(" BROX@> "),
+  style:{
+    bg: '#F21B2D',
+  }
 });
 
 const logsMain = blessed.log({
-  top: '0%+2', 
+  parent: box,
+  top: '20%', 
   left: '2%',
   width: '50%', 
-  height: 22,
+  height: '60%',
   border: 'line',
-  scrollable: true,
-  label:'Terminal'
+  label: 'Logs',
+});
+
+const line = blessed.line({
+  parent: box,
+  orientation: 'horizontal',
+  top: 1,
+  left: 'center',
+  width: '95%',
+
+});
+
+const boxAtalhos = blessed.box({
+  parent: box,
+  top: 0,
+  left: 'center',
+  width: '50%',
+  height: 1,
+  style:{
+    bg: '#f21b2d',
+  }
+});
+
+const atalhos = blessed.text({
+  parent: boxAtalhos,
+  top: 'center',
+  left: 'center',
+  width: 'shrink',
+  height: 1,
+  tags: true,
+  content: '{#f21b2d-bg}ESC:{/#f21b2d-bg}'+'{bold}{#262626-bg} Sair {/#262626-bg}{/bold}'+'{#f21b2d-bg}|{/#f21b2d-bg}'+'{#f21b2d-bg}F1:{/#f21b2d-bg}'+'{bold}{#262626-bg} Ajuda {/#262626-bg}{/bold}',
+  style:{
+    fg: '#D9D9D9',
+  }
+});
+
+const titulo = blessed.bigtext({
+  parent: box,
+  top: 'center',
+  left: 'center',
+  width: '20%',
+  height: 3,
+  content: texto,
+  style: {
+    fg: '#F21B2D',
+  }
 });
 
 /*funções */
 function resetarInput() {
-  input.setValue(giz.white.bgGreen.bold("BROX@>"));
+  input.setValue('');
   screen.render();
   input.focus();
 }
 
 /*eventos*/
-box.key(["escape", "C-c"], function (ch, key) {
+box.key(["escape", "C-c", 'q'], function (ch, key) {
   return process.exit(0);
 });
 
-input.key("enter", () => {
-  const texto = input.getValue().slice(35);
-  const info = box.getContent();
-  const logtxt = '@\>';
-  logsMain.add(`${logtxt} ${info} ${texto}`);
-  resetarInput();
+input.key(["escape", "C-c"], function (ch, key) {
+  return process.exit(0);
 });
-input.key("backspace", () => {
+input.key("enter", () => {
   const texto = input.getValue();
-  if (texto.length == 34) {
-    resetarInput();
-  }
+  const logtxt = '@\>';
+  logsMain.log(`${logtxt} ${texto}`);
+  resetarInput();
 });
 
 /*Adiciona os elementos à tela */
 screen.append(box);
-screen.append(input);
-screen.append(logsMain);
-
 
 /*Renderiza a tela e foca no input */
 screen.render();
